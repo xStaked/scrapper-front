4a22fcaa04c3ebe1530d5915b64aad8739448039
xstaked <aljndr911@gmail.com>
2024-02-17 22:38:40 -0500

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Input } from "./components/ui/input";
+
 function App() {
   return (
     <>
-      <h1 className="text-4xl font-bold text-center">Scrapper front</h1>
+      <div className="dark bg-slate-950 text-white w-screen h-screen flex flex-col items-center justify-center gap-4">
+        <h1 className="text-4xl font-bold text-center">Scrapper front</h1>
+        <Input
+          type="text"
+          className="w-1/2"
+          placeholder="Search for something"
+        />
+      </div>
     </>
   );
 }
